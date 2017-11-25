b36ddb2b3094ccf343a9ed3e54ca18449acb8f0b
Eddy Ernesto del Valle Pino <xigmatron@gmail.com>
2017-11-25 20:42:18 +0100

    Add support for parameter autocompletion.

--- super_elixir/autocomplete.py
+++ super_elixir/autocomplete.py
@@ -16,14 +16,29 @@ class Autocomplete(sublime_plugin.EventListener):
         if not is_elixir(view):
             return
 
-        buffer, line, column = get_buffer_line_column(view, locations[0])
+        location = locations[0]
+        on_character =  view.substr(location)
+        prev_char = view.substr(location - 1)
+
+        param_auto_completion = (
+            on_character in FOLLOWING_CHARS and
+            prev_char == '('
+        )
+
+        if param_auto_completion:
+            location -= 1
+
+        f_name = view.substr(view.word(location))
+        buffer, line, column = get_buffer_line_column(view, location)
 
         sense = get_elixir_sense(view)
         suggestions = sense.suggestions(buffer, line, column)
 
         completions = []
         for s in suggestions:
-            if s['type'] == 'hint':
+            if (s['type'] == 'hint' or
+                    param_auto_completion and
+                    s['name'] != f_name):
                 continue
 
             c = {
@@ -38,41 +53,37 @@ class Autocomplete(sublime_plugin.EventListener):
                 'completion': s['name'],
             }
 
-            if s['type'].endswith('function') or s['type'] == 'macro':
-                args = s.get('args', '')
+            if self._is_function(s):
                 arity = s.get('arity')
-                if args:
-                    args = args.split(',')
-                else:
-                    args = []
+                args = s.get('args', '').split()
 
                 if not args and arity:
                     args = ['_'] * arity
 
-                c['show'] += '(' + ', '.join(args) + ')'
-
                 completion_args = [
-                    '${%s:%s}' % (i, a)
-                    for i, a in enumerate(args, 1)
+                    '${%s:%s}' % (i, a) for i, a in enumerate(args, 1)
                 ]
-                c['completion'] += '(' + ', '.join(completion_args) + ')'
+
+                if param_auto_completion:
+                    c['show'] = ', '.join(args)
+                    c['completion'] = ', '.join(completion_args)
+                else:
+                    c['show'] += '(' + ', '.join(args) + ')'
+                    c['completion'] += '(' + ', '.join(completion_args) + ')'
 
             completions.append(c)
 
-        completions = self._sort_by_frequency_in_view(buffer, completions)
         completions = [
             ['{show}\t{hint}'.format(**c), c['completion']]
             for c in completions
         ]
         return completions
 
-    def _sort_by_frequency_in_view(self, buffer, completions):
-        completions.sort(key=lambda c: (
-            -buffer.count(c['name']),  # how many times it is in the buffer
-            len(c['name']) - len(c['name'].strip('_')),  # less underscores wins
-            c['name'],  # alphabetically
-        ))
-        return completions
+    def _is_function(self, suggestion):
+        return (
+            suggestion['type'].endswith('function') or
+            suggestion['type'] == 'macro'
+        )
 
     def on_hover(self, view, point, hover_zone):
         if hover_zone == sublime.HOVER_TEXT and is_elixir(view):
@@ -101,19 +112,18 @@ class Autocomplete(sublime_plugin.EventListener):
 
 class SuperElixirParamsAutocomplete(sublime_plugin.TextCommand):
 
-    def run(self, edit, characters='('):
-
-        point = self.view.sel()[0].a
-        f_name = self.view.substr(self.view.word(point))
-        completions = Autocomplete().on_query_completions(
-            self.view, f_name, [point])
-
-        if completions:
-            show, snippet = completions[0]
-            snippet = snippet[snippet.index('('):]  # cut out the function name
-            self.view.run_command('insert_snippet', {"contents": snippet})
-        else:
-            self._insert_characters(edit, characters, ')')
+    def run(self, edit):
+        self._insert_characters(edit, '(', ')')
+        sublime.set_timeout(
+            (lambda: self.view.run_command(
+                'auto_complete',
+                {
+                    'next_completion_if_showing': False,
+                    'auto_complete_commit_on_tab': True,
+                }
+            )),
+            0
+        )
 
     @property
     def auto_match_enabled(self):
